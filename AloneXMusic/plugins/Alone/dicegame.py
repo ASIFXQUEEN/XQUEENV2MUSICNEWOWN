221925d313d7b202593caeb399171be87c07f22f
RAK RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-06 22:20:21 +0530

    Update dicegame.py

--- AloneXMusic/plugins/Alone/dicegame.py
+++ AloneXMusic/plugins/Alone/dicegame.py
@@ -10,24 +10,23 @@ from pyrogram.handlers import MessageHandler
 async def dice(bot, message):
     x=await bot.send_dice(message.chat.id)
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"Dekh {message.from_user.mention} Tera Kya aya : {m}",quote=True)
   
 @app.on_message(filters.command("dart"))
 async def dart(bot, message):
     x=await bot.send_dice(message.chat.id, "🎯")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
-
+    await message.reply_text(f"Dekh {message.from_user.mention} tera nishana mc : {m}",quote=True)
 @app.on_message(filters.command("basket"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🏀")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"madrchod {message.from_user.mention} telegram ko kya socha tune : {m}",quote=True)
 @app.on_message(filters.command("jackpot"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎰")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"Madrchd {message.from_user.mention} jakpot teri ma ko de : {m}",quote=True)
 @app.on_message(filters.command("ball"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎳")
