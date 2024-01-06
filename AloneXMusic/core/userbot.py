0367e75b756c0f91efd4c3b96872a0476f6546c9
RAK RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-06 22:09:42 +0530

    Update userbot.py

--- AloneXMusic/core/userbot.py
+++ AloneXMusic/core/userbot.py
@@ -51,13 +51,13 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("BESTODISHA")
+                await self.one.join_chat("ARAME9")
             except:
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.one.send_message(config.LOGGER_ID, "Misqueen started...")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -67,18 +67,18 @@ class Userbot(Client):
             self.one.name = self.one.me.mention
             self.one.username = self.one.me.username
             assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
+            LOGGER(__name__).info(f"Misqueen Started as {self.one.name}")
 
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.two.join_chat("BESTODISHA")
+                await self.one.join_chat("ARAME9")
             except:
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.two.send_message(config.LOGGER_ID, "Misqueen Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
@@ -88,21 +88,21 @@ class Userbot(Client):
             self.two.name = self.two.me.mention
             self.two.username = self.two.me.username
             assistantids.append(self.two.id)
-            LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
+            LOGGER(__name__).info(f"Misqueen Two Started as {self.two.name}")
 
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.three.join_chat("BESTODISHA")
+                await self.one.join_chat("ARAME9")
             except:
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.three.send_message(config.LOGGER_ID, "Misqueen Started")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "Misqueen 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
                 exit()
             self.three.id = self.three.me.id
@@ -114,16 +114,16 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.four.join_chat("BESTODISHA")
+                await self.one.join_chat("ARAME9")
             except:
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.four.send_message(config.LOGGER_ID, "Misqueen Started")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "Misqueen Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
                 exit()
             self.four.id = self.four.me.id
@@ -135,16 +135,16 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("AlonesHeaven")
-                await self.one.join_chat("AloneXBots")
+                await self.five.join_chat("BESTODISHA")
+                await self.one.join_chat("ARAME9")
             except:
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant Started")
+                await self.five.send_message(config.LOGGER_ID, "Misqueen started...")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    "Misqueen Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
                 )
                 exit()
             self.five.id = self.five.me.id
@@ -154,7 +154,7 @@ class Userbot(Client):
             LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
 
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping Assistants...")
+        LOGGER(__name__).info(f"Bye naveen mu tg delete karuchi...")
         try:
             if config.STRING1:
                 await self.one.stop()
