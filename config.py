d03aa0230f8ac17a8ca2759f127c153fc65c1d72
RAK RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-07 05:39:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ASIFXQUEEN/PATHAAN",
+    "https://github.com/ASIFXQUEEN/XQUEENV2MUSICNEWOWN",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
