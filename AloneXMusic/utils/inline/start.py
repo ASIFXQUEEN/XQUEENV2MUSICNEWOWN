60188a25578989b8b805fcfade24f02968174b1f
RAK RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-08 21:24:06 +0530

    Update start.py

--- AloneXMusic/utils/inline/start.py
+++ AloneXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
